a8434dfa4535ce13c15759916cadb2d049dac7b8
JefryOne <Korniytsov.sv@gmail.com>
2024-10-24 18:56:20 +0300

    Добавил тест plugin

---
+++ src/test/java/com/example/finance/ExampleTest.java
@@ -0,0 +1,12 @@
+package com.example.finance;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ExampleTest {
+
+    @Test
+    public void testExample() {
+        assertTrue(true);
+    }
+}
